6b7ec7c350d33e284a882ec5fde9616b64e8aeeb
Peter Desmet <peter.desmet.work@gmail.com>
2019-09-17 11:53:48 +0200

    Write download_detections script
    Loops over animal_ids and outputs csv file

---
+++ src/functions/download_detections.R
@@ -0,0 +1,38 @@
+download_detections <- function(sql_file, download_directory, animal_ids,
+                         connection, overwrite = FALSE) {
+  # Check input arguments
+  if (!file.exists(sql_file)) {
+    stop("No such sql_file: ", sql_file)
+  }
+
+  if (!file.exists(download_directory)) {
+    stop("No such directory: ", download_directory)
+  }
+
+  # Loop over animal_ids
+  for (animal_id in animal_ids) {
+    # Create file name
+    detections_file = file.path(download_directory, paste0("dwc_occ_detections_", animal_id, ".csv"))
+
+    # Query and download data
+    if (file.exists(detections_file) && !overwrite) {
+      print(paste(animal_id, ": ", detections_file, "already exists, skipping download"))
+    } else {
+      print(paste(animal_id, ": downloading data"))
+      gps_sql <- glue_sql(read_file(sql_file), .con = connection)
+      detections <- tryCatch({
+        dbGetQuery(con, gps_sql)
+      }, error = function(e) {
+        return(NA)
+      })
+
+      if (nrow(detections) != 0 && is.na(detections)) {
+        # detections = NA will be the case if there was an error in SQL conn.
+        # break loop and don't write to file
+        break
+      } else {
+        write_csv(detections, path = detections_file, na = "")
+      }
+    }
+  }
+}
