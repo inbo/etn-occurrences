8800da26956d1b92390ce63069a5920d6b1865f2
lienreyserhove <lien.reyserhove@inbo.be>
2019-09-23 15:10:10 +0200

    Finalize first mapping version for dwc_event_deployments

--- sql/dwc_event_deployments.sql
+++ sql/dwc_event_deployments.sql
@@ -15,11 +15,32 @@
 -- Other
 
 SELECT
- 'urn:catalog:etn:receiver:' || receiver_fk || ':event-receiver-deployment' as eventID,
- CASE
-  WHEN recover_date_time IS NULL THEN deploy_date_time AT TIME ZONE 'UTC'
-  ELSE to_char(recover_date_time, 'HH12:MI:SS')
-  END AS eventDate
+
+  'Event'::text as type,
+  'en'::text as language,
+  'http://creativecommons.org/publicdomain/zero/1.0/'::text as license,
+  'INBO'::text as rightsholder,
+  'http://www.inbo.be/en/norms-for-data-use'::text as accessRights,
+  ''::text as datasetID,
+  'Acoustic telemetry data of fish in the Scheldt river basin and the Belgian Part of the North Sea (BPNS)'::text as datasetName,
+  'INBO'::text as institutionCode,
+  'MachineObservation'::text as basisOfRecord,
+  'urn:catalog:etn:receiver:' || receiver_fk || ':event-receiver-deployment' as eventID,
+  '' as samplingProtocol,
+    CASE
+      WHEN recover_date_time IS NULL THEN (to_char(deploy_date_time, 'YYYY-MM-DD'))::text
+      ELSE (
+          to_char(deploy_date_time, 'YYYY-MM-DD') ||
+          '/' ||
+          to_char(recover_date_time, 'YYYY-MM-DD')
+          )::text
+      END AS eventDate,
+  '' as locality,
+  ROUND(deploy_lat::numeric,5) as decimalLatitude,
+  ROUND(deploy_long::numeric,5) as decimalLongitude,
+  'WGS84'::text as geodeticDatum,
+  30::numeric as coordinateUncertaintyInMeters
+
 FROM vliz.deployments_view AS deployments
 WHERE projectcode = 'albert'
   OR projectname = 'bovenschelde'
