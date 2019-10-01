8252fa6b5b8e382cfce6c99cfbfaf080b3e286a5
lienreyserhove <lien.reyserhove@inbo.be>
2019-10-01 11:18:59 +0200

    Generate dwc_animals_occ.sql

--- sql/dwc_animals_occ.sql
+++ sql/dwc_animals_occ.sql
@@ -1,3 +1,65 @@
--- !preview conn=DBI::dbConnect(RSQLite::SQLite())
 
-SELECT 1
+SELECT
+  eventID,
+  occurrenceID,
+  person_id::text as recordedBy,
+  scientific_name::text as scientificName,
+  'Animalia'::text as kingdom,
+  'Chordata'::text as phylum,
+  common_name::text as vernacularName,
+  'species'::text as taxonRank
+
+FROM (
+
+  (SELECT
+
+  -- Capture event
+
+    'urn:catalog:etn:' || projectcode || ':' || id_pk || ':event-capture' as eventID,
+    'urn:catalog:etn:' || projectcode || ':' || id_pk || ':occurrence-capture' as occurrenceID,
+    *
+
+  FROM vliz.animals_view
+
+  UNION ALL
+
+  SELECT
+
+  -- Surgery event
+
+    'urn:catalog:etn:' || projectcode || ':' || id_pk || ':event-surgery' as eventID,
+    'urn:catalog:etn:' || projectcode || ':' || id_pk || ':occurrence-capture' as occurrenceID,
+    *
+
+  FROM vliz.animals_view
+
+  UNION ALL
+
+  SELECT
+
+-- Release event
+
+  'urn:catalog:etn:' || projectcode || ':' || id_pk || ':event-release' as eventID,
+  'urn:catalog:etn:' || projectcode || ':' || id_pk || ':occurrence-capture' as occurrenceID,
+  *
+
+
+ FROM vliz.animals_view
+
+  )
+
+) AS x
+
+
+    WHERE
+
+      (projectcode = '2011_rivierprik'
+        OR projectcode = '2012_leopoldkanaal'
+        OR projectcode = '2013_albertkanaal'
+        OR projectcode = '2014_demer'
+        OR projectcode = '2015_dijle'
+        OR (projectcode = '2015_phd_verhelst' AND scientific_name = 'Anguilla anguilla')
+        OR projectcode = 'homarus'
+        OR projectcode = 'phd_reubens')
+      AND NOT scientific_name = 'Sentinel'
+      AND NOT scientific_name = 'Sync tag'
