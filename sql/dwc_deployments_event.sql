8cc6476191f399b416cfcda1d1365828a40c6e57
lienreyserhove <lien.reyserhove@inbo.be>
2019-10-01 15:52:48 +0200

    Improve dwc_deployments_event.sql code + update associated csv file

--- sql/dwc_deployments_event.sql
+++ sql/dwc_deployments_event.sql
@@ -16,22 +16,27 @@
 
 SELECT
 
---- Metadata terms:
+-- Metadata terms:
 
   'Event'::text as type,
   'en'::text as language,
   'http://creativecommons.org/publicdomain/zero/1.0/'::text as license,
-  'INBO'::text as rightsholder,
+  ''::text as rightsholder,
   'http://www.inbo.be/en/norms-for-data-use'::text as accessRights,
   ''::text as datasetID,
   'Acoustic telemetry data of fish in the Scheldt river basin and the Belgian Part of the North Sea (BPNS)'::text as datasetName,
-  'INBO'::text as institutionCode,
+  ''::text as institutionCode,
   'MachineObservation'::text as basisOfRecord,
 
---- Taxon Core terms:
+-- Taxon Core terms:
 
-  'urn:catalog:etn:receiver:' || receiver_fk || ':event-receiver-deployment' as eventID,
+  --- eventID
+ 'urn:catalog:etn:' || projectcode || ':' || id_pk || ':event-receiver-deployment' as eventID,
+
+  --- samplingProtocol
   '' as samplingProtocol,
+
+  --- eventDate
     CASE
       WHEN recover_date_time IS NULL THEN (to_char(deploy_date_time, 'YYYY-MM-DD'))::text
       ELSE (
@@ -40,23 +45,40 @@ SELECT
           to_char(recover_date_time, 'YYYY-MM-DD')
           )::text
       END AS eventDate,
-  '' as locality,
+
+  --- locatlity
+  station_name::text as locality,
+
+  --- decimalLatitude
   ROUND(deploy_lat::numeric,5) as decimalLatitude,
+
+  --- decimalLongitude
   ROUND(deploy_long::numeric,5) as decimalLongitude,
-  'WGS84'::text as geodeticDatum,
-  30::numeric as coordinateUncertaintyInMeters
+
+  --- eventRemarks
+ CASE
+  WHEN recover_lat IS NOT NULL THEN
+    ('recover latitude: ' || ROUND(recover_lat::numeric,5) || ' | ' ||
+    'recover longitude: ' || ROUND(recover_long::numeric,5) || ' | ' ||
+    'location name: ' || location_name || ' | ' ||
+    'location description: ' || COALESCE(location_description, 'NA'))::text
+  ELSE
+     ('location name: ' || location_name || ' | ' ||
+    'location description: ' || COALESCE(location_description, 'NA'))::text
+  END AS eventRemarks
 
 FROM vliz.deployments_view AS deployments
 
-WHERE projectcode = 'albert'
-    OR projectcode = 'bovenschelde'
-    OR projectcode = 'bpns'
-    OR projectcode = 'demer'
-    OR projectcode = 'dijle'
-    OR projectcode = 'leopoldkanaal'
-    OR projectcode = 'maas'
-    OR projectcode = 'saeftinghe'
-    OR projectcode = 'ws1'
-    OR projectcode = 'ws2'
-    OR projectcode = 'ws3'
+  WHERE projectcode = 'albert'
+      OR projectcode = 'bovenschelde'
+      OR projectcode = 'bpns'
+      OR projectcode = 'demer'
+      OR projectcode = 'dijle'
+      OR projectcode = 'leopoldkanaal'
+      OR projectcode = 'maas'
+      OR projectcode = 'saeftinghe'
+      OR projectcode = 'ws1'
+      OR projectcode = 'ws2'
+      OR projectcode = 'ws3'
+
 
