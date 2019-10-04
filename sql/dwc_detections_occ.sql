df0d2ef0e21433fc8bbb9991369bff3542321d1f
lienreyserhove <lien.reyserhove@inbo.be>
2019-10-04 09:52:47 +0200

    Finish dwc_occ_detections.sql and generate examples 728 and 729

--- sql/dwc_detections_occ.sql
+++ sql/dwc_detections_occ.sql
@@ -1,7 +1,19 @@
 
-SELECT *
+SELECT
+   'urn:catalog:etn:' || network_project_code || ':' || deployment_fk || ':occurrence-detections'::text as eventID,
+   'urn:catalog:etn:' || animal_project_code || ':' || animal_id_pk || ':' || 'id_pk' || ':occurrence-detections'::text as occurrenceID,
+   ''::text as recordedBy,
+   scientific_name::text as scientificName,
+   'Animalia'::text as kingdom,
+   'Chordata'::text as phylum,
+   animal_common_name::text as vernacularName,
+   'species'::text as taxonRank
+
 FROM vliz.detections_view
 
+WHERE animal_id_pk = {animal_id}
+
+LIMIT 10
 
 
 
