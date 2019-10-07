d8caefb5363567bc17a301c227b574cec39060bc
lienreyserhove <lien.reyserhove@inbo.be>
2019-10-07 12:01:44 +0200

    Add eventDate to detection occurrence mapping

--- sql/dwc_detections_occ.sql
+++ sql/dwc_detections_occ.sql
@@ -1,12 +1,14 @@
 
 SELECT
    'urn:catalog:etn:' || network_project_code || ':' || deployment_fk || ':occurrence-detections'::text as eventID,
-   'urn:catalog:etn:' || animal_project_code || ':' || animal_id_pk || ':' || 'id_pk' || ':occurrence-detections'::text as occurrenceID,
+   'urn:catalog:etn:' || animal_project_code || ':' || animal_id_pk || ':' || id_pk || ':occurrence-detections'::text as occurrenceID,
+   to_char(datetime, 'YYYY-MM-DD') as eventDate,
    ''::text as recordedBy,
    scientific_name::text as scientificName,
    'Animalia'::text as kingdom,
    'Chordata'::text as phylum,
    animal_common_name::text as vernacularName,
+   animal_sex::text as sex,
    'species'::text as taxonRank
 
 FROM vliz.detections_view
