4f6d14bd08e797746ca1d7735d0faeed80ca751b
lienreyserhove <lien.reyserhove@inbo.be>
2019-10-08 15:24:41 +0200

    Improve mapping script for occ_detections

--- sql/dwc_occ_detections.sql
+++ sql/dwc_occ_detections.sql
@@ -1,9 +1,8 @@
 
 SELECT
-   'urn:catalog:etn:' || network_project_code || ':' || deployment_fk || ':occurrence-detections'::text as eventID,
-   'urn:catalog:etn:' || animal_project_code || ':' || animal_id_pk || ':' || id_pk || ':occurrence-detections'::text as occurrenceID,
+   'etn:' || network_project_code || ':' || deployment_fk || ':event-receiver-deployment'::text as eventID,
+   'etn:' || animal_project_code || ':' || animal_id_pk || ':' || id_pk || ':occurrence-detection'::text as occurrenceID,
    to_char(datetime, 'YYYY-MM-DD') as eventDate,
-   ''::text as recordedBy,
    scientific_name::text as scientificName,
    'Animalia'::text as kingdom,
    'Chordata'::text as phylum,
