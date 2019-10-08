24f96f91ffa042f63d774c7244292f26c44c8f04
lienreyserhove <lien.reyserhove@inbo.be>
2019-10-08 15:50:45 +0200

    Update dwc_occ_detections - Improve eventDate - Remove unnecessary columns - Remove limit

--- sql/dwc_occ_detections.sql
+++ sql/dwc_occ_detections.sql
@@ -2,19 +2,13 @@
 SELECT
    'etn:' || network_project_code || ':' || deployment_fk || ':event-receiver-deployment'::text as eventID,
    'etn:' || animal_project_code || ':' || animal_id_pk || ':' || id_pk || ':occurrence-detection'::text as occurrenceID,
-   to_char(datetime, 'YYYY-MM-DD') as eventDate,
+   to_char(datetime, 'YYYY-MM-DD"T"HH24:MI:SS"Z"') as eventDate,
    scientific_name::text as scientificName,
    'Animalia'::text as kingdom,
-   'Chordata'::text as phylum,
-   animal_common_name::text as vernacularName,
-   animal_sex::text as sex,
-   'species'::text as taxonRank
 
 FROM vliz.detections_view
 
 WHERE animal_id_pk = {animal_id}
-
-LIMIT 10
 
 
 
